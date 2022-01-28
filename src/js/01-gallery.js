606d4862c0fc18f8cda15f664b0c7989dcd4a8e5
Nadia Lut <3197344@gmail.com>
2022-01-28 22:42:07 +0200

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// console.log(galleryItems);
+
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+// const gallaryEl = document.querySelector('.gallery');
+// const create = galleryItems.map(
+//   ({ preview, original, description }) =>
+//     `<a class="gallery__item" href="${original}">
+//   <img class="gallery__image" src="${preview}" alt="${description}"/>
+// </a>`,
+// );
+// gallaryEl.insertAdjacentHTML('afterbegin', create.join(''));
+// var lightbox = new SimpleLightbox('.gallery a', {});
+// console.log(galleryItems);
+
+
+const galleryBoxEl = document.querySelector('.gallery');
+const galleryMarkup = galleryItems
+    .map(
+        ({ preview, original, description }) =>
+        ` <a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`,
+        
+    )
+    .join('');
+
+galleryBoxEl.insertAdjacentHTML('afterbegin', galleryMarkup); 
+const lightbox = new SimpleLightbox('.gallery a', {});
 console.log(galleryItems);
