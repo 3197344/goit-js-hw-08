f88f096240a194e28e5cc7d073c4d6e9de308b04
Nadia Lut <3197344@gmail.com>
2022-01-28 22:46:53 +0200

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,19 +9,6 @@ import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-
-// const gallaryEl = document.querySelector('.gallery');
-// const create = galleryItems.map(
-//   ({ preview, original, description }) =>
-//     `<a class="gallery__item" href="${original}">
-//   <img class="gallery__image" src="${preview}" alt="${description}"/>
-// </a>`,
-// );
-// gallaryEl.insertAdjacentHTML('afterbegin', create.join(''));
-// var lightbox = new SimpleLightbox('.gallery a', {});
-// console.log(galleryItems);
-
-
 const galleryBoxEl = document.querySelector('.gallery');
 const galleryMarkup = galleryItems
     .map(
